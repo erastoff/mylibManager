83539a0e0370df2580255588d31660147cead59c
erastoff <yuriy.erastov@yandex.ru>
2024-07-24 15:26:44 +0300

    tests completed

--- test_main.py
+++ test_main.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import functools
 import unittest
+from unittest.mock import patch
+from io import StringIO
 
 from main import Book, Library
 
@@ -22,7 +24,7 @@ class TestBooks(unittest.TestCase):
     def setUp(self):
         self.test_title = "test_title"
         self.test_author = "test_author"
-        self.test_year = 2000
+        self.test_year = 1111
         self.test_status = "test_status"
 
         self.new_book = Book(
@@ -86,19 +88,48 @@ class TestLibrary(unittest.TestCase):
     def setUp(self):
         self.test_library = Library("test_library.json")
 
+        self.test_title = "test_title"
+        self.test_author = "test_author"
+        self.test_year = 1111
+        self.test_status = "test_status"
+
     def test_existent_library(self):
         self.assertTrue(isinstance(self.test_library, Library))
         self.assertEqual(len(self.test_library.books), 4)
-        # params = (
-        #     {
-        #         "author": "Дмитрий Глуховский",
-        #     },
-        # )
-        for book in self.test_library.books:
-            pass
 
-    def test_not_existent_library(self):
-        pass
+    @patch.object(Book, "generate_id", return_value=12345)
+    def test_add_and_delete_book(self, mock_generate_id):
+        self.test_library.add_book(
+            title=self.test_title,
+            author=self.test_author,
+            year=self.test_year,
+        )
+        self.assertEqual(len(self.test_library.books), 5)
+        self.test_library.delete_book(12345)
+        self.assertEqual(len(self.test_library.books), 4)
+
+    @patch("sys.stdout", new_callable=StringIO)
+    @cases(
+        [
+            {
+                "title": "Harry Potter",
+            },
+            {
+                "author": "J.K. Rowling",
+            },
+            {
+                "year": 2005,
+            },
+        ]
+    )
+    def test_search_book(self, mock_stdout, arguments):
+        self.test_library.search_books(**arguments)
+        output = mock_stdout.getvalue()
+        self.assertIn("Harry Potter", output)
+        self.assertIn("J.K. Rowling", output)
+        self.assertIn("2005", output)
+        self.assertIn("J.K. Rowling", output)
+        self.assertIn("Status: в наличии", output)
 
 
 if __name__ == "__main__":
